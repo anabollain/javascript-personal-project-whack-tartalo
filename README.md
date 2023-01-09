01c2651d04154713b8964df5948da80d9fe24304
Ana Bollain <ana.bollain@gmail.com>
2023-01-09 18:03:18 +0100

    Upload project

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# javascript-personal-project-whack-tartalo
+# Whack Tartalo
+
+## How It Works
+
+Let's play! The game is based on the famous **Whack a mole** game, but with customized aesthetics inspired in Basque Mythology and one of its most representative figures: **Tartalo**. If you are not familiar with the game, the rules are very simple; you must hit as many moles, in this case Tartalos, as you can before the timer runs out. 
+
+Rules and settings:
+
+* You can choose the number of **seconds** for the game (between 10 and 30)
+* You can also set your preferred **speed** (low, medium or speedy)
+* Once you have chosen the settings, you can start the game by clicking in the **Start** button below
+* The timer decreases by 1s once you have clicked the button
+* Each Tartalo you whack is worth 1 point
+* At the end of the game, a pop up will appear with your final result
+
+You will find the **score** beneath the board, as well as the number of seconds left.
+
+You can reset the game by clicking in **Try again** and start over with the previous or new settings
+
+## What I've practiced
+
+* querySelector()
+* addEventListener()
+* setInterval()
+* classList
+* forEach()
+* Arrow functions
+* Advanced DOM
+* Transitions
+
+## Web Application
+
+You will find below some images of each state of the application, as well as some additional information:
+
+![My Image](images/site.JPG)
+
+*With:*
+*- The game board*
+*- A score result*
+*- A time counter*
+*- Radio inputs to select the time*
+*- Radio inputs to select the speed*
+*- A start button*
+*- An article with information about Tartalo*
+*- A link to learn more about him*
+
+
+Once you click the start button, Tartalo will appear randomly in the board game, and his aspect will change when hovering and whacking him: 
+
+![My Image](images/site-game.JPG)
+
+
+When there are no more seconds left, a pop up will appear with your final result. The site will return to its initial state when clicking in try again and you will be able to start a new game:
+
+![My Image](images/site-restart.JPG)
+
+## Credits
+
+* [learn-javascript-by-building-games](https://www.freecodecamp.org/news/learn-javascript-by-building-7-games-video-course/)
+
---
+++ main.js
@@ -0,0 +1,137 @@
+'use strict';
+
+//All divs with square class
+const squareEl = document.querySelectorAll('.square');
+//All divs with mole class
+const timeLeft = document.querySelector('#js-whack-time-left');
+const scoreEl = document.querySelector('#js-whack-score');
+//Start button
+const startEl = document.querySelector('#js-whack-start');
+//All radio inputs for speed
+const inputEl = document.querySelectorAll('.js-whack-input');
+//All radio inputs for time interval
+const timeEl = document.querySelectorAll('.js-whack-int');
+
+let result = 0;
+let hitPosition = 0;
+let currentTime = 10;
+let timerId = null;
+let timeInterval = 1000;
+
+adjustTime();
+
+//Function to randomly select a square in the grid
+function randomSquare() {
+    //Remove mole class from all divs in our grid
+    squareEl.forEach(className => {
+        className.classList.remove('mole');
+    })
+    let randomSquare = squareEl[Math.floor(Math.random()*9)];
+    randomSquare.classList.add('mole');
+    //Assign the id of the randomPosition to hitPosition to use it later
+    hitPosition = randomSquare.id;
+}
+
+//Add even listener to each square in the grid
+squareEl.forEach(square => {
+    square.addEventListener('click', (event) =>{
+        if(square.id === hitPosition){
+            result += 1;
+            scoreEl.innerHTML = result;
+        }
+    })
+})
+
+//Function to change speed
+function adjustSpeed() {
+    //Add event listener to each input
+    inputEl.forEach(input=> {
+        input.addEventListener('click', () => {
+            if(input.id === 'low'){
+                timeInterval = 1000;
+            }else if(input.id === 'medium'){
+                timeInterval = 800;
+            }else if(input.id === 'super'){
+                timeInterval = 600;
+            }
+        })
+    })
+}
+
+//Function to change time interval
+function adjustTime() {
+    //Add event listener to each input
+    timeEl.forEach(input=> {
+        input.addEventListener('click', () => {
+            if(input.id === '10'){
+                currentTime = 10;
+                timeLeft.innerHTML = currentTime;
+            }else if(input.id === '20'){
+                currentTime = 20;
+                timeLeft.innerHTML = currentTime;
+            }else if(input.id === '30'){
+                currentTime = 30;
+                timeLeft.innerHTML = currentTime;
+            }
+        })
+    })
+}
+
+
+let countDownTimerId = null;
+//Function for time left
+function countDown() {
+    //Decrease value by one
+    currentTime--;
+    //Adjust time
+    adjustTime();
+    console.log(currentTime)
+    //Update timer
+    timeLeft.innerHTML = currentTime;
+    if(currentTime === 0){
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        showAlert(result);
+    }
+}
+
+//Function to adjust speed by users choice
+adjustSpeed();
+
+startEl.addEventListener('click', () => {
+    adjustTime();
+    result = 0;
+    //Function to move mole randomly
+    console.log(timeInterval)
+    timerId = setInterval(randomSquare, timeInterval);
+    countDownTimerId = setInterval(countDown, 1000);
+})
+
+
+function showAlert(value) {
+    const alertBox = document.createElement('div');
+    alertBox.setAttribute('class', 'js-alert-box');
+    const alertBoxText = document.createElement('p');
+    alertBoxText.setAttribute('class', 'js-alert-text')
+    const alertBoxMsg = document.createTextNode('Game is over! Your final result is ' + value);
+    alertBoxText.appendChild(alertBoxMsg);
+    const alertBtn = document.createElement('button');
+    alertBtn.setAttribute('class', 'js-alert-btn');
+    const alertBtnMsg = document.createTextNode('Try again');
+    alertBtn.appendChild(alertBtnMsg);
+    alertBox.appendChild(alertBoxText);
+    alertBox.appendChild(alertBtn);
+    const body = document.querySelector('body');
+    body.appendChild(alertBox);
+    alertBtn.addEventListener('click', ()=>{
+        console.log('hide')
+        console.log(alertBox)
+        alertBox.classList.add('js-remove');
+        alertBox.classList.remove('js-alert-box');
+        result = 0;
+        scoreEl.innerHTML = result;
+        squareEl.forEach(className => {
+            className.classList.remove('mole');
+        })
+    })
+}
